a06eacf517d89b9141b046aad063d9ff07a58923
tomjschwanke <tomschwanke80@gmail.com>
2020-08-04 21:21:24 +0200

    send message to player about cost and limit that to once every 10ms

--- src/main/java/de/tomjschwanke/unlimitedenchantment/AnvilEvents.java
+++ src/main/java/de/tomjschwanke/unlimitedenchantment/AnvilEvents.java
@@ -1,5 +1,6 @@
 package de.tomjschwanke.unlimitedenchantment;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -7,10 +8,16 @@ import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 
 public class AnvilEvents implements Listener {
+    long lastCalled = System.currentTimeMillis();
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void anvilCost(PrepareAnvilEvent event) {
+        Player p = (Player) event.getViewers().get(0);
         AnvilInventory inv = event.getInventory();
         inv.setMaximumRepairCost(Integer.MAX_VALUE);
+        if(event.getResult() != null && System.currentTimeMillis() - lastCalled > 10) {
+            lastCalled = System.currentTimeMillis();
+            p.sendMessage("Cost: " + inv.getRepairCost());
+        }
     }
 }
