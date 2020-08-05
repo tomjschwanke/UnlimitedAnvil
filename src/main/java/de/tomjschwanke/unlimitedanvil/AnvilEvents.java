01b73ce596f080c33b359e32e8946fb704e9027a
tomjschwanke <tomschwanke80@gmail.com>
2020-08-05 23:24:10 +0200

    clarified comment

--- src/main/java/de/tomjschwanke/unlimitedanvil/AnvilEvents.java
+++ src/main/java/de/tomjschwanke/unlimitedanvil/AnvilEvents.java
@@ -22,7 +22,7 @@ public class AnvilEvents implements Listener {
             Only send a message about cost if
             1. both slots are filled,
             2. no msg was sent in the last 10ms (this gets called 3 times so it would send 3 messages) and
-            3. only if the cost is >= 40 since otherwise the anvil would still show cost
+            3. only if the cost is >= 40 since a cost higher than 40 will result in the anvil still showing "too expensive", regardless of set maximum repair cost
         */
         if(event.getResult() != null && System.currentTimeMillis() - lastCalled > 10 && inv.getRepairCost() >= 40) {
             // Set last called for 10ms cooldown (see above)
